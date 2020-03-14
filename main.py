f236bec042a85135aefe6b7ebdb7b06fe0766cdd
uts58 <utsha.saha.58@gmail.com>
2020-03-14 18:37:07 +0600

    some works

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
 
 if __name__ == '__main__':
-    res=requests.get('https://www.dsebd.org/latest_share_price_all.php')
+    temp = list()
+    res = requests.get('https://www.dsebd.org/latest_share_price_all.php')
+    a = res.text.split('</table>')[0].replace('''<td align='right'>''', '').replace('''<td align='center'>''', '').replace('''</tr>''', '').split('''<tr bgcolor='#EFEFEF'>''')[1:]
+    # for items in a:
+    #     temp.append(items.split('</td>')[:-1])
+    #     # print(items)
+    print(a[0].split('</td>')[1].replace("""""<td> <a href='""", ''))
+    print(a[0].split('</td>')[:-1])
