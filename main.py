27891d56ddb051359d6aba0d07999b884f0c671c
uts58 <utsha.saha.58@gmail.com>
2020-03-14 21:54:22 +0600

    6.5 mb of ram usage xDDDDD

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ import requests
 
 if __name__ == '__main__':
     temp = list()
+    data = dict()
     res = requests.get('https://www.dsebd.org/latest_share_price_all.php')
-    a = res.text.split('</table>')[0].replace('''<td align='right'>''', '').replace('''<td align='center'>''', '').replace('''</tr>''', '').split('''<tr bgcolor='#EFEFEF'>''')[1:]
-    # for items in a:
-    #     temp.append(items.split('</td>')[:-1])
-    #     # print(items)
-    print(a[0].split('</td>')[1].replace("""""<td> <a href='""", ''))
-    print(a[0].split('</td>')[:-1])
+    text = res.text.split('''<tr bgcolor='#EFEFEF'>''')[1:]
+    for items in text:
+        sanitizedText = items.replace('''<td align='right'>''', '').replace("""</font>""", '').replace('''<td align='center'>''', '').replace('''</tr>''', '').replace("""<td> <a href='""", '').replace("""</a>""", '').split('</td>')[:-1]
+        sanitizedText[1] = sanitizedText[1].split("""' target='_top' class='ab1'>""")
+        data[sanitizedText[1][1]] = {
+            'id': sanitizedText[0],
+            'url': 'https://www.dsebd.org/' + sanitizedText[1][0],
+            'ltp': sanitizedText[2],
+            'high': sanitizedText[3],
+            'low': sanitizedText[4],
+            'closeep': sanitizedText[5],
+            'ycp': sanitizedText[6],
+            'change': sanitizedText[7].split("""'>""")[1],
+            'trade': sanitizedText[8],
+            'value (mn)': sanitizedText[9],
+            'volume': sanitizedText[10].replace(',', '')
+        }
+
+print(data)
