3654857271db9d5c18840000356fa74b434fc641
uts58 <utsha.saha.58@gmail.com>
2020-03-14 22:03:18 +0600

    preparing rocket science

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import requests
 if __name__ == '__main__':
     temp = list()
     data = dict()
-    res = requests.get('https://www.dsebd.org/latest_share_price_all.php')
+    res = requests.get('https://www.dsebd.org/latest_share_price_all.php', timeout=(5, 5))
     text = res.text.split('''<tr bgcolor='#EFEFEF'>''')[1:]
     for items in text:
         sanitizedText = items.replace('''<td align='right'>''', '').replace("""</font>""", '').replace('''<td align='center'>''', '').replace('''</tr>''', '').replace("""<td> <a href='""", '').replace("""</a>""", '').split('</td>')[:-1]
-        sanitizedText[1] = sanitizedText[1].split("""' target='_top' class='ab1'>""")
-        data[sanitizedText[1][1]] = {
+        data[sanitizedText[1].split("""' target='_top' class='ab1'>""")[1]] = {
             'id': sanitizedText[0],
-            'url': 'https://www.dsebd.org/' + sanitizedText[1][0],
+            'url': 'https://www.dsebd.org/' + sanitizedText[1].split("""' target='_top' class='ab1'>""")[0],
             'ltp': sanitizedText[2],
             'high': sanitizedText[3],
             'low': sanitizedText[4],
